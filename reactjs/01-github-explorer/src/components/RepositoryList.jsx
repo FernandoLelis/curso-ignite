40964eeefbb39a63d4c9fd283189c60d56ded0ed
FernandoLelis <fernandolelisdarocha@gmail.com>
2022-05-16 23:38:48 -0300

    Primeiro componete React

---
+++ reactjs/01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,52 @@
+const repositoryName = 'Unform2';
+
+export function RespositoryList() {
+    return (
+        <section className="respository-list">
+            <h1>Lista de Repositorios</h1>
+
+            <ul>
+                <li>
+                    <strong>{repositoryName}</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar Repositorios
+                    </a>
+                </li>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar Repositorios
+                    </a>
+                </li>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar Repositorios
+                    </a>
+                </li>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar Repositorios
+                    </a>
+                </li>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar Repositorios
+                    </a>
+                </li>
+            </ul>
+        </section>
+    );
+}

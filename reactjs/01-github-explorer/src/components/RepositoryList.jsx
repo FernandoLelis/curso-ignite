2bf8fab9b1df7b85d96ebc810edad26d59f819f1
FernandoLelis <fernandolelisdarocha@gmail.com>
2022-05-19 16:33:50 -0300

    Listando repositórios

--- reactjs/01-github-explorer/src/components/RepositoryList.jsx
+++ reactjs/01-github-explorer/src/components/RepositoryList.jsx
@@ -4,12 +4,6 @@ import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 
 
-const repository = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: 'https://github.com/unform/unform'
-}
-
 export function RespositoryList() {
     const [repositories, setRepositories] = useState([]);
 
@@ -24,10 +18,9 @@ export function RespositoryList() {
             <h1>Lista de Repositorios</h1>
 
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                })}
             </ul>
         </section>
     );

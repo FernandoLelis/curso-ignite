cfdf547b4543dfd300702f6194ca099021a583f9
FernandoLelis <fernandolelisdarocha@gmail.com>
2022-06-29 11:37:01 -0300

    CSS Modules

--- reactjs-atualizado/01-fundamentos-reactjs/src/App.jsx
+++ reactjs-atualizado/01-fundamentos-reactjs/src/App.jsx
@@ -1,7 +1,13 @@
+import { Header } from './components/Header';
+import './styles.css';
+
 function App() {
   return (
-    <h1>Hello Word</h1>
-  )
+    <div>
+      <Header />
+      <h1>Hello Word</h1>
+    </div>
+  );
 }
 
 export default App

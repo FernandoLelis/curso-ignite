a84f429977a980f11e2e04d5bf9a1cd9aabb3507
FernandoLelis <fernandolelisdarocha@gmail.com>
2022-07-01 18:00:40 -0300

    Component: Sidebar

--- reactjs-atualizado/01-fundamentos-reactjs/src/App.jsx
+++ reactjs-atualizado/01-fundamentos-reactjs/src/App.jsx
@@ -1,11 +1,22 @@
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
+
 import './global.css';
+
 
 function App() {
   return (
     <div>
       <Header />
-      <h1>Hello Word</h1>
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <h1>Hello Word</h1>
+        </main>
+      </div>
     </div>
   );
 }

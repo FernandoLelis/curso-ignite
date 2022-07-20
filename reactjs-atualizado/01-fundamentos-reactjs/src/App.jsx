454870f89c51d12c67c23ff01e789838507f7fe3
FernandoLelis <fernandolelisdarocha@gmail.com>
2022-07-20 17:21:44 -0300

    Interando no JSX

--- reactjs-atualizado/01-fundamentos-reactjs/src/App.jsx
+++ reactjs-atualizado/01-fundamentos-reactjs/src/App.jsx
@@ -6,7 +6,36 @@ import styles from './App.module.css';
 
 import './global.css';
 
-
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/FernandoLelis.png',
+      name: 'Fernando Lélis',
+      role: 'Desenvolvedor Web',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-07-20 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/FernandoLelis.png',
+      name: 'Fernando',
+      role: 'Desenvolvedor Web',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-07-20 20:00:00'),
+  },
+]
 
 function App() {
   return (
@@ -16,8 +45,15 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            ) 
+          })}
         </main>
       </div>
     </div>

de0a2a9e83d0687d952dcccd120effd30fdceda8
FernandoLelis <fernandolelisdarocha@gmail.com>
2022-09-19 17:51:25 -0300

    Estilos globais

--- reactjs-atualizado/02-ignite-timer/src/App.tsx
+++ reactjs-atualizado/02-ignite-timer/src/App.tsx
@@ -1,6 +1,9 @@
 import { ThemeProvider } from 'styled-components';
 import { Button } from './components/Button';
+
+
 import { defaultTheme } from './Styles/themes/default';
+import { GlobalStyle } from './Styles/themes/global';
 
 
 export function App() {
@@ -11,6 +14,8 @@ export function App() {
       <Button variant="success" />
       <Button variant="danger" />
       <Button />
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }

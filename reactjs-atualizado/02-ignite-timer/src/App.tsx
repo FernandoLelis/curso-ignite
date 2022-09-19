7325fa096ddd625969a4de77eaae86961cf1a7e3
FernandoLelis <fernandolelisdarocha@gmail.com>
2022-09-19 12:17:27 -0300

    Styled Components

--- reactjs-atualizado/02-ignite-timer/src/App.tsx
+++ reactjs-atualizado/02-ignite-timer/src/App.tsx
@@ -1,9 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { Button } from './components/Button'
 
 
 export function App() {
   return (
-    <h1>Hello Workd!</h1>
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+    </>
   )
 }

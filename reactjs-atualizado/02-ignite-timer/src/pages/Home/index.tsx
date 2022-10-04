0f79bc23b79eb232bb305d9ba9a2453375ba5f27
FernandoLelis <fernandolelisdarocha@gmail.com>
2022-10-04 11:54:32 -0300

    Aprimorando os inputs

--- reactjs-atualizado/02-ignite-timer/src/pages/Home/index.tsx
+++ reactjs-atualizado/02-ignite-timer/src/pages/Home/index.tsx
@@ -11,13 +11,24 @@ export function Home() {
                 <TaskInput 
                     placeholder="Dê um nome para o seu projeto" 
                     id="task"
+                    list="task-suggestions"
                 />
+
+                <datalist id="task-suggestions">
+                    <option value="Projeto 01" />
+                    <option value="Projeto 02" />
+                    <option value="Projeto 03" />
+                    <option value="Casa" />
+                </datalist>
 
                 <label htmlFor="minutesAmount">Durante</label>
                 <MinutesAmountInput 
                     type="number" 
                     id="minutesAmount" 
                     placeholder="00" 
+                    step={5}
+                    min={5}
+                    max={60}
                 /> 
 
                 <span>minutos.</span>

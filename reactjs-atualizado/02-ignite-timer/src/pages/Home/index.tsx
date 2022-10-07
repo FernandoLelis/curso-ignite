f1d8fbb8b58f2c7caf8a0eff09eb8c1e83311126
FernandoLelis <fernandolelisdarocha@gmail.com>
2022-10-07 17:50:49 -0300

    Validando formulário

--- reactjs-atualizado/02-ignite-timer/src/pages/Home/index.tsx
+++ reactjs-atualizado/02-ignite-timer/src/pages/Home/index.tsx
@@ -1,19 +1,31 @@
 import { Play } from "phosphor-react";
 import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as zod from 'zod';
 
 import { 
-        CountdownContainer, 
-        FormContainer, 
-        HomeContainer, 
-        MinutesAmountInput, 
-        Separetor, 
-        StartCountdownButton, 
-        TaskInput 
-    } from "./styles";
+    CountdownContainer, 
+    FormContainer, 
+    HomeContainer, 
+    MinutesAmountInput, 
+    Separetor, 
+    StartCountdownButton, 
+    TaskInput 
+} from "./styles";
     
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, 'Informe a tarefa'),
+    minutesAmount: zod
+        .number()
+        .min(5, 'O ciclo precisa ser de no minimo 5 minutos')
+        .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
+})
+
 
 export function Home() {
-    const { register, handleSubmit, watch }  = useForm()
+    const { register, handleSubmit, watch }  = useForm({
+        resolver: zodResolver(newCycleFormValidationSchema),
+    })
 
     function handleCreateNewCycle(data: any) {
         console.log(data);        

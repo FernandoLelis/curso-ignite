c053e1927488e86ed1bc97cb2d453e8e2e65b418
FernandoLelis <fernandolelisdarocha@gmail.com>
2022-10-10 17:40:54 -0300

    Resetando formulário

--- reactjs-atualizado/02-ignite-timer/src/pages/Home/index.tsx
+++ reactjs-atualizado/02-ignite-timer/src/pages/Home/index.tsx
@@ -25,7 +25,7 @@ type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 
 export function Home() {
-    const { register, handleSubmit, watch }  = useForm<NewCycleFormData>({
+    const { register, handleSubmit, watch, reset }  = useForm<NewCycleFormData>({
         resolver: zodResolver(newCycleFormValidationSchema),
         defaultValues: {
           task: '',
@@ -34,7 +34,8 @@ export function Home() {
     })
 
     function handleCreateNewCycle(data: NewCycleFormData) {
-        console.log(DataView);        
+        console.log(data);
+        reset();        
     }
 
     const task = watch('task')

eaad6452f613629a295a8ed050ebb95a8090ab9b
FernandoLelis <fernandolelisdarocha@gmail.com>
2022-12-09 17:18:31 -0300

    Modal de nova transação

--- reactjs-atualizado/03-dt-money/src/components/Header/index.tsx
+++ reactjs-atualizado/03-dt-money/src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import * as Dialog from '@radix-ui/react-dialog';
 
 import logoImg from "../../assets/logo.svg";
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
     return (
@@ -12,14 +13,9 @@ export function Header() {
                 <Dialog.Root>
                     <Dialog.Trigger asChild>
                         <NewTransactionButton>Nova transação</NewTransactionButton>
-                    </Dialog.Trigger>
-                    <Dialog.Portal>
-                        <Dialog.Overlay />
-                        <Dialog.Content>
-                            <Dialog.Title>Nova transação</Dialog.Title>
-                            <Dialog.Close />
-                        </Dialog.Content>
-                    </Dialog.Portal>
+                    </Dialog.Trigger>  
+
+                    <NewTransactionModal />                  
                 </Dialog.Root>                
             </HeaderContent>
         </HeaderContainer>

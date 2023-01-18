f8feaaa8584b78182a9cf495a3c427bb1c860607
FernandoLelis <fernandlelisdarocha@gmail.com>
2023-01-18 12:17:09 -0300

    Navegação via Link

--- reactjs-atualizado/04-ignite-shop/src/pages/index.tsx
+++ reactjs-atualizado/04-ignite-shop/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from "next";
 import Image from "next/image";
+import Link from "next/link";
 
 import { useKeenSlider } from 'keen-slider/react';
 
@@ -32,14 +33,16 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
+          <Link key={product.id} href={`/product/${product.id}`} >
+            <Product className="keen-slider__slide" >
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-            <footer>
-              <strong>{product.name}</strong>
-                <span>{product.price}</span>          
-            </footer>
-          </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                  <span>{product.price}</span>          
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
@@ -58,7 +61,10 @@ export const getStaticProps: GetStaticProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount! / 100,
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(price.unit_amount! / 100),
     }
   })  
 

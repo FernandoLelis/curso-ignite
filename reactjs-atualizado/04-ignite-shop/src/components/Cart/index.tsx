622ae36ad63b37a9e39699a45d66ef7867a9f9e6
FernandoLelis <fernandlelisdarocha@gmail.com>
2023-02-02 11:48:25 -0300

    Estilização do modal carrinho de compras finalizado

--- reactjs-atualizado/04-ignite-shop/src/components/Cart/index.tsx
+++ reactjs-atualizado/04-ignite-shop/src/components/Cart/index.tsx
@@ -2,7 +2,7 @@ import * as Diolog from '@radix-ui/react-dialog';
 import Image from 'next/image';
 import { X } from "phosphor-react";
 import { CartButton } from "../CartButton";
-import { CartClose, CartContent, CartProduct, CartProductImage } from './styles';
+import { CartClose, CartContent, CartFinalization, CartProduct, CartProductDetails, CartProductImage, FinalizationDetails } from './styles';
 
 export function Cart() {
     return (
@@ -31,8 +31,29 @@ export function Cart() {
                                     src="https://s3-alpha-sig.figma.com/img/387d/13ce/de131bd1ccf9bbe6b2331e88d3df20cd?Expires=1676246400&Signature=mU-mXt5DGYNDRpMwLXoUS6PHnt-UQDUzvQMWkE~J1evMIB6Fk4~-U6XBr~M8Q6UtUZdtxuBHjeuX4oE9ONO~1stFepXnUk10J3QKvgqR2QiaUf3qkVDrayd7T4MzSQnAECR60luM4T3rBkXUNIec2OXyHaQ-jK3d8vbX5xzlkvZ6q6OgrLZM2IOp0J7dOVFd02vqy9M~e4zaVWMzToeOFGVPHlVjpqg5zS6Qrp-or7FORrNIfeAWOAWu5p2lFGCqE2iXRFdUPHG3Z7YKhCtD7FybbDLv963KTlS03iJfyS9ROQe8gR~vsm7QTXpFkrzHssSeioyMOTBdFdDR~0ksmg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
                                 />
                             </CartProductImage>
+
+                            <CartProductDetails>
+                                <p>Produto 1</p>
+                                <strong> R$50,00</strong>
+                                <button>Revover</button>
+                            </CartProductDetails>
                         </CartProduct>
                     </section>
+
+                    <CartFinalization>
+                        <FinalizationDetails>
+                            <div>
+                                <span>Quantidade</span>
+                                <p>2 itens</p>
+                            </div>
+                            <div>
+                                <span>Valor total</span>
+                                <p>R$100,00</p>
+                            </div>
+                        </FinalizationDetails>
+
+                        <button>Finalizar compra</button>
+                    </CartFinalization>
                 </CartContent>
             </Diolog.Portal>
         </Diolog.Root>

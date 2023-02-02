2a8af4ee5653e5ceb3b6c707f30fb707a2d99648
FernandoLelis <fernandlelisdarocha@gmail.com>
2023-02-02 16:01:27 -0300

    slider modificado

--- reactjs-atualizado/04-ignite-shop/src/pages/index.tsx
+++ reactjs-atualizado/04-ignite-shop/src/pages/index.tsx
@@ -3,14 +3,11 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 
-import { useKeenSlider } from 'keen-slider/react';
-
-
 import { stripe } from "../lib/stripe";
-import { HomeContainer, Product } from "../styles/pages/home";
+import { HomeContainer, Product, SliderContainer } from "../styles/pages/home";
 
-import 'keen-slider/keen-slider.min.css';
 import Stripe from "stripe";
+import useEmblaCarousel from "embla-carousel-react";
 
 
 interface HomeProps {
@@ -23,12 +20,11 @@ interface HomeProps {
 }
 
 export default function Home({ products }: HomeProps) {
-  const [sliderRef] = useKeenSlider({
-    slides: {
-      perView: 3,
-      spacing: 48,
-    }
-  });
+  const [emblaRef] = useEmblaCarousel({
+    align: 'start',
+    skipSnaps: false,
+    dragFree: true,
+  })
 
   return (
     <>
@@ -36,22 +32,28 @@ export default function Home({ products }: HomeProps) {
         <title>Home | Ignite Shop</title>
       </Head>
       
-      <HomeContainer ref={sliderRef} className="keen-slider">
-        {products.map(product => {
-          return (
-            <Link key={product.id} href={`/product/${product.id}`} prefetch={false} >
-              <Product className="keen-slider__slide" >
-                <Image src={product.imageUrl} width={520} height={480} alt="" />
+      <div style={{ overflow: 'hidden', width: '100%'}}> 
+        <HomeContainer>
+          <div className="embla" ref={emblaRef}>
+            <SliderContainer className="embla__container container">
+              {products.map(product => {
+                return (
+                  <Link key={product.id} href={`/product/${product.id}`} prefetch={false} >
+                    <Product className="embla__slide" >
+                      <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-                <footer>
-                  <strong>{product.name}</strong>
-                    <span>{product.price}</span>          
-                </footer>
-              </Product>
-            </Link>
-          )
-        })}
-      </HomeContainer>
+                      <footer>
+                        <strong>{product.name}</strong>
+                          <span>{product.price}</span>          
+                      </footer>
+                    </Product>
+                  </Link>
+                )
+              })}
+            </SliderContainer>
+          </div>
+        </HomeContainer>
+      </div>
     </>
   )
 }

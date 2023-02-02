57dd187857ab5d05a3f7d931f4348a1cd52dd31b
FernandoLelis <fernandlelisdarocha@gmail.com>
2023-02-02 17:35:01 -0300

    Variantes do botão criadas

--- reactjs-atualizado/04-ignite-shop/src/pages/index.tsx
+++ reactjs-atualizado/04-ignite-shop/src/pages/index.tsx
@@ -8,6 +8,7 @@ import { HomeContainer, Product, SliderContainer } from "../styles/pages/home";
 
 import Stripe from "stripe";
 import useEmblaCarousel from "embla-carousel-react";
+import { CartButton } from "../components/CartButton";
 
 
 interface HomeProps {
@@ -43,8 +44,11 @@ export default function Home({ products }: HomeProps) {
                       <Image src={product.imageUrl} width={520} height={480} alt="" />
 
                       <footer>
-                        <strong>{product.name}</strong>
-                          <span>{product.price}</span>          
+                        <div>
+                          <strong>{product.name}</strong>
+                          <span>{product.price}</span> 
+                        </div>
+                        <CartButton color="green" size="large" />         
                       </footer>
                     </Product>
                   </Link>

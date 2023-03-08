59c7d4fcf6c6c77f95c7eca97109ddd87d8eb8dd
FernandoLelis <fernandolelisdarocha@gmail.com>
2023-03-08 07:03:35 -0300

    Component: Text Em andamento

---
+++ reactjs-atualizado/05-design-system/packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,15 @@
+import type { StoryObj, Meta } from '@storybook/react';
+import { Text, TextProps } from '@ignite-ui/react';
+
+export default {
+    title: 'Typography/Text',
+    component: Text,
+
+    args: {
+        children: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam magni exercitationem minus error vel, magnam quod amet harum debitis itaque incidunt quas nisi voluptatibus quis ea earum qui vitae libero."
+    },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {
+    
+}
